d16f5d4680febac61a6e248a9844016ef670b545
hezhenwei <3711971@qq.com>
2018-04-25 10:52:55 +0800

    Update font.go

--- x/image/font/font.go
+++ x/image/font/font.go
@@ -7,7 +7,7 @@
 //
 // Other packages provide font face implementations. For example, a truetype
 // package would provide one based on .ttf font files.
-package font // import "golang.org/x/image/font"
+package font // import "github.com/hezhenwei/golang/x/image/font"
 
 import (
 	"image"
@@ -15,7 +15,7 @@ import (
 	"io"
 	"unicode/utf8"
 
-	"../../image/math/fixed"
+	"github.com/hezhenwei/golang/x/image/math/fixed"
 )
 
 // TODO: who is responsible for caches (glyph images, glyph indices, kerns)?
